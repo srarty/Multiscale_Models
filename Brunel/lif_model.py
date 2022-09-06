f547f4cde50a9208e19834ce25369d90b5fb1736
srarty <srarty@users.noreply.github.com>
2022-09-06 11:33:53 +1000

    lif_model cleanup

--- Brunel/lif_model.py
+++ Brunel/lif_model.py
@@ -26,8 +26,88 @@ def set_params(type='pyramidal', source='brunel'):
             source: Reference for the parameters ('brunel'-from Callavari 2014, 'allen'-from billeh 2020 )
     '''
     
-    if type == 'pyramidal':
-        if source == 'brunel':
+    if source == 'allen':
+        #%% Allen parameters
+        # Connectivity
+        p_IP =  0.411
+        p_PI =  0.395
+        p_PP =  0.16
+        p_II =  0.451#0.125 #0.19#0.451
+        
+        if type == 'pyramidal':
+            #%% Pyramidal. Allen
+            g_leak = 25 * nS # Leak conductance
+            C = 0.5 * nF # Membrane capacitance
+            
+            tau_rpa = 2 * ms # Absolute refractory period
+            tau_rpr = 100 * ms # Relative refractory period
+            tau_rp = tau_rpa + tau_rpr # Effective refractory period
+            tau_m = 20 * ms # Membrane time constant
+    
+            tau_GABA_r = 0.25 * ms
+            tau_GABA_d = 5 * ms
+            tau_AMPA_r = 0.4 * ms
+            tau_AMPA_d = 2 * ms
+            tau_AMPA_r_ext = 0.4 * ms
+            tau_AMPA_d_ext = 2 * ms
+            tau_l = 1 * ms # Latency
+    
+            # Synaptic efficacies
+            j_GABA  = 37 * pA # 74 * pA # NMM(5.01) = LIF(335*pA)
+            j_AMPA  = -73.5 * pA #-147 * pA # NMM(5.003) = LIF(-890*pA)
+            j_AMPA_ext = -1.375 * pA
+            j_AMPA_tha = -13.75 * pA
+            
+            # Delta function weight (increment with each input spike)
+            # Defined experimentally with 'synaptic_functions.py'. Based on the 
+            # unitary increment of the single exponential.
+            alpha_simple_weight_AMPA = 1
+            alpha_simple_weight_GABA = 1
+            alpha_simple_weight_AMPA_ext = 12.5
+            single_exponential_weight = 8.2
+            
+        elif type == 'inhibitory':            
+            #%% Inhibitory. Allen
+            g_leak = 20 * nS # Leak conductance
+            C = 0.2 * nF # Membrane capacitance
+    
+            tau_rpa = 1 * ms # Absolute refractory period
+            tau_rpr = 100 * ms # Relative refractory period
+            tau_rp = tau_rpa + tau_rpr # Effective refractory period
+            tau_m = C/g_leak #10 * ms # Membrane time constant
+    
+            tau_GABA_r = 0.25 * ms
+            tau_GABA_d = 5 * ms
+            tau_AMPA_r = 0.2 * ms # 3 * ms  # <- bifurcation # 0.2 * ms
+            tau_AMPA_d = 1 * ms # 15 * ms # <- bifurcation # 1 * ms
+            tau_AMPA_r_ext = 0.2*ms 
+            tau_AMPA_d_ext = 1*ms 
+            tau_l = 1 * ms # Latency
+    
+            # Synaptic efficacies
+            j_GABA  = 17.55 * pA # 35.1 * pA # Default = 35.1*pA # NMM(5.008) = LIF(61*pA)
+            j_AMPA  = -165 * pA # -330 * pA # Default = -330*pA # NMM(5.009) = LIF(-690*pA)
+            j_AMPA_ext = -1.9 * pA
+            j_AMPA_tha = -19 * pA
+            
+            # Delta function weight (increment with each input spike)
+            alpha_simple_weight_AMPA = 1
+            alpha_simple_weight_GABA = 1
+            alpha_simple_weight_AMPA_ext = 23
+            single_exponential_weight = 8.2
+            
+        else:
+            return 0
+            
+    elif source == 'brunel':
+        #%% Brunel parameters
+        # Connectivity
+        p_IP =  0.2
+        p_PI =  0.2
+        p_PP =  0.2
+        p_II =  0.2
+        
+        if type == 'pyramidal':
             #%% Pyramidal. Brunel
             g_leak = 25 * nS # Leak conductance
             C = 0.5 * nF # Membrane capacitance
@@ -52,49 +132,12 @@ def set_params(type='pyramidal', source='brunel'):
             # Delta function weight (increment with each input spike)
             # Defined experimentally with 'synaptic_functions.py'. Based on the 
             # unitary increment of the single exponential.
-            single_exp_weight = 1 # Inverse: 1/1.52 = 0.6578947368421053
             alpha_simple_weight_AMPA = 12.5
             alpha_simple_weight_GABA = 1
             alpha_simple_weight_AMPA_ext = 12.5
             single_exponential_weight = 8.2
-            delayed_exp_weight_AMPA = 6.0995 # Inverse 1.52/6.0995 = 0.24920075416017706
-            delayed_exp_weight_GABA = 6.0995 # TODO
             
-        else: # if 'allen'
-            #%% Pyramidal. Allen
-            g_leak = 25 * nS # Leak conductance
-            C = 0.5 * nF # Membrane capacitance
-    
-            tau_rp = 2 * ms # Absolute refractory period
-            tau_m = 20 * ms # Membrane time constant
-    
-            tau_GABA_r = 0.25 * ms
-            tau_GABA_d = 5 * ms
-            tau_AMPA_r = 0.4 * ms
-            tau_AMPA_d = 2 * ms
-            tau_AMPA_r_ext = 0.4 * ms
-            tau_AMPA_d_ext = 2 * ms
-            tau_l = 1 * ms # Latency
-    
-            # Synaptic efficacies
-            j_GABA  = 37 * pA # 74 * pA # NMM(5.01) = LIF(335*pA)
-            j_AMPA  = -73.5 * pA #-147 * pA # NMM(5.003) = LIF(-890*pA)
-            j_AMPA_ext = -1.375 * pA
-            j_AMPA_tha = -13.75 * pA
-            
-            # Delta function weight (increment with each input spike)
-            # Defined experimentally with 'synaptic_functions.py'. Based on the 
-            # unitary increment of the single exponential.
-            single_exp_weight = 1 # TODO
-            alpha_simple_weight_AMPA = 1
-            alpha_simple_weight_GABA = 1
-            alpha_simple_weight_AMPA_ext = 12.5 # TODO
-            single_exponential_weight = 8.2 # TODO
-            delayed_exp_weight_AMPA = 6.0995 # TODO
-            delayed_exp_weight_GABA = 6.0995 # TODO
-        
-    elif type == 'inhibitory':
-        if source == 'brunel':
+        elif type == 'inhibitory':    
             #%% Inhibitory. Brunel
             g_leak = 20 * nS # Leak conductance
             C = 0.2 * nF # Membrane capacitance
@@ -117,57 +160,19 @@ def set_params(type='pyramidal', source='brunel'):
             j_AMPA_tha = -19 * pA
             
             # Delta function weight (increment with each input spike)
-            single_exp_weight = 1
             alpha_simple_weight_AMPA = 13 # 2.85
             alpha_simple_weight_GABA = 1.25 # 0.69
             alpha_simple_weight_AMPA_ext = 23
             single_exponential_weight = 8.2
-            delayed_exp_weight_AMPA = 27.96 
-            delayed_exp_weight_GABA = 39
-        else: 
-            #%% Inhibitory. Allen
-            g_leak = 20 * nS # Leak conductance
-            C = 0.2 * nF # Membrane capacitance
-    
-            tau_rp = 1 * ms # Absolute refractory period
-            tau_m = C/g_leak #10 * ms # Membrane time constant
-    
-            tau_GABA_r = 0.25 * ms
-            tau_GABA_d = 5 * ms
-            tau_AMPA_r = 0.2 * ms # 3 * ms  # <- bifurcation # 0.2 * ms
-            tau_AMPA_d = 1 * ms # 15 * ms # <- bifurcation # 1 * ms
-            tau_AMPA_r_ext = 0.2*ms 
-            tau_AMPA_d_ext = 1*ms 
-            tau_l = 1 * ms # Latency
-    
-            # Synaptic efficacies
-            j_GABA  = 17.55 * pA # 35.1 * pA # Default = 35.1*pA # NMM(5.008) = LIF(61*pA)
-            j_AMPA  = -165 * pA # -330 * pA # Default = -330*pA # NMM(5.009) = LIF(-690*pA)
-            j_AMPA_ext = -1.9 * pA
-            j_AMPA_tha = -19 * pA
             
-            # Delta function weight (increment with each input spike)
-            single_exp_weight = 1
-            alpha_simple_weight_AMPA = 1
-            alpha_simple_weight_GABA = 1
-            alpha_simple_weight_AMPA_ext = 23
-            single_exponential_weight = 8.2
-            delayed_exp_weight_AMPA = 1 
-            delayed_exp_weight_GABA = 1
+        else:
+            return 0
+        
     else:
-        return 0
+         return 0   
+           
+    
 
-    #%% Connectivity
-    if source == 'brunel':
-        p_IP =  0.2
-        p_PI =  0.2
-        p_PP =  0.2
-        p_II =  0.2
-    else:
-        p_IP =  0.411
-        p_PI =  0.395
-        p_PP =  0.16
-        p_II =  0.451#0.125 #0.19#0.451
 
     #%% Parse output
     params = {
